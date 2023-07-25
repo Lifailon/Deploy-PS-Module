9fb11c20799a1a51b1e26daa4ef775cfc1ef42bb
Lifailon <lifailon@yandex.ru>
2023-07-25 19:18:10 +0300

    test

--- Scripts/Deploy-Module.ps1
+++ Scripts/Deploy-Module.ps1
@@ -1,7 +1,8 @@
+$Module_Path = "C:\Program Files\PowerShell\7\Modules"
 $Name_Module = "pSyslog"
 $Temp_Path = "$env:TEMP\$Name_Module.tar.gz"
-$Module_Path = "C:\Program Files\PowerShell\7\Modules"
 $GitHub_Rep = "https://api.github.com/repos/Lifailon/pSyslog/tarball"
+Get-ChildItem $Module_Path\$Name_Module
 
 Invoke-WebRequest -URI $GitHub_Rep -outfile $Temp_Path # download
 Set-Location $env:TEMP
@@ -9,6 +10,7 @@ tar -xzf $Temp_Path # extract gzip
 $Path_Arch = (Get-ChildItem *$Name_Module* -Directory).FullName
 Remove-Item "$Module_Path\$Name_Module" -Recurse -Force
 Copy-Item -Path "$Path_Arch\Module\pSyslog" -Destination "$Module_Path\" -Recurse
+Get-ChildItem $Module_Path\$Name_Module
 
 Remove-Item $Temp_Path
 Remove-Item $Path_Arch -Recurse
