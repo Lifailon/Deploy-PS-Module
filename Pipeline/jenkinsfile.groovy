334c4abb9894af21845d60d6197d5d87966f65ec
Lifailon <lifailon@yandex.ru>
2023-07-25 17:54:59 +0300

    test

--- Pipeline/jenkinsfile.groovy
+++ Pipeline/jenkinsfile.groovy
@@ -3,12 +3,12 @@ pipeline {
   stages {
     stage('Check Version') {
       steps {
-        sh 'ansible-playbook test/Module-Version.yml'
+        sh 'ansible-playbook Pipeline/Module-Version.yml'
       }
     }
     stage('Deploy Module') {
       steps {
-        sh 'ansible-playbook test/Module-Deploy.yml'
+        sh 'ansible-playbook Pipeline/Module-Deploy.yml'
       }
     }
   }
