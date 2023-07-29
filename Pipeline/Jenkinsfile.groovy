f3527fe7051974d4303a40f76e4920bb95f6b2b0
Lifailon <lifailon@yandex.ru>
2023-07-29 11:33:36 +0300

    test

--- Pipeline/Jenkinsfile.groovy
+++ Pipeline/Jenkinsfile.groovy
@@ -7,7 +7,7 @@ pipeline {
     choice(name: "State", choices: ["None","Start","Restart","Stop"], description: "Select state")
   }
   stages {
-    stage('State service') {
+    stage('Ansible and PowerShell') {
       steps {
         echo "Name of the selected service: $params.ServiceName"
         // pass parameters to the input of ansible variables
