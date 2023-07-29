55fcf47a490f0b91bee03a4e99e97485d0c1c7c8
Lifailon <lifailon@yandex.ru>
2023-07-29 11:03:06 +0300

    test

--- Pipeline/Jenkinsfile.groovy
+++ Pipeline/Jenkinsfile.groovy
@@ -3,13 +3,14 @@ pipeline {
   parameters {
     // creat parameters
     string(name: "ServiceName", defaultValue: "ssh", trim: true, description: "Enter the name of the service")
-    booleanParam(name: "ChangeState", defaultValue: false)
-    choice(name: "State", choices: ["Stop","Restart","Start"], description: "Select state")
+    // booleanParam(name: "ChangeState", defaultValue: false)
+    choice(name: "State", choices: ["None","Start","Restart","Stop"], description: "Select state")
   }
   stages {
     stage('Before Deploy Version') {
       steps {
         echo "Name of the selected service: $params.ServiceName"
+        // pass parameters to the input of ansible variables
         sh "ansible-playbook Pipeline/Get-Service.yml -e 'ServiceName=$params.ServiceName State=$params.State'"
       }
     }
