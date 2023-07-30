5238e2c240ce0efb590ca327ec2213ae46b7cd0e
Lifailon <lifailon@yandex.ru>
2023-07-31 01:22:47 +0300

    test state

--- Pipeline/Jenkinsfile.groovy
+++ Pipeline/Jenkinsfile.groovy
@@ -1,7 +1,6 @@
 pipeline {
   agent any
   parameters {
-    // creat parameters
     string(name: "ServiceName", defaultValue: "sshd", trim: true, description: "Enter the name of the service")
     // booleanParam(name: "ChangeState", defaultValue: false)
     choice(name: "State", choices: ["None","Start","Restart","Stop"], description: "None - only get status")
@@ -12,8 +11,10 @@ pipeline {
     stage('PowerShell via Ansible') {
       steps {
         echo "Name of the selected service: $params.ServiceName"
-        // pass parameters to the input of ansible variables
-        sh "ansible-playbook Pipeline/Get-Service.yml -e 'ServiceName=$params.ServiceName State=$params.State StartType=$params.StartType System=$params.System '"
+        If [[ $params.System -eq "Windows" ]]
+        Then sh "ansible-playbook Pipeline/Get-Service.yml -e 'ServiceName=$params.ServiceName State=$params.State StartType=$params.StartType'"
+        Else echo "Linux"
+        Fi
       }
     }
   }
