3336b02401a266080b9d14b3528edc03aa6ffa6b
Lifailon <lifailon@yandex.ru>
2023-07-30 13:54:19 +0300

    test state

--- Pipeline/Jenkinsfile.groovy
+++ Pipeline/Jenkinsfile.groovy
@@ -4,6 +4,7 @@ pipeline {
     // creat parameters
     string(name: "ServiceName", defaultValue: "sshd", trim: true, description: "Enter the name of the service")
     // booleanParam(name: "ChangeState", defaultValue: false)
+    choice(name: "System", choices: ["Windows","Linux"], description: "Select the operating system")
     choice(name: "State", choices: ["None","Start","Restart","Stop"], description: "None - only get status")
     choice(name: "StartType", choices: ["None","Automatic","AutomaticDelayedStart","Disabled","Manual"], description: "Delayed Start mode supported only PowerShell 7")
   }
@@ -12,7 +13,7 @@ pipeline {
       steps {
         echo "Name of the selected service: $params.ServiceName"
         // pass parameters to the input of ansible variables
-        sh "ansible-playbook Pipeline/Get-Service.yml -e 'ServiceName=$params.ServiceName State=$params.State StartType=$params.StartType'"
+        sh "ansible-playbook Pipeline/Get-Service.yml -e 'ServiceName=$params.ServiceName System=$params.System State=$params.State StartType=$params.StartType'"
       }
     }
   }
