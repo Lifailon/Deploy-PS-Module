87008b00c68ce08ef121b0de2b38b784210e149c
Lifailon <lifailon@yandex.ru>
2023-07-30 03:11:31 +0300

    test state

--- Pipeline/Jenkinsfile.groovy
+++ Pipeline/Jenkinsfile.groovy
@@ -5,7 +5,7 @@ pipeline {
     string(name: "ServiceName", defaultValue: "sshd", trim: true, description: "Enter the name of the service")
     // booleanParam(name: "ChangeState", defaultValue: false)
     choice(name: "State", choices: ["None","Start","Restart","Stop"], description: "None - only get status")
-    choice(name: "StartType", choices: ["None","Automatic","AutomaticDelayedStart","Disabled","Manual"])
+    choice(name: "StartType", choices: ["None","Automatic","AutomaticDelayedStart","Disabled","Manual"], description: "Delayed Start mode supported only PowerShell 7")
   }
   stages {
     stage('PowerShell via Ansible') {
