6c55b6d9e1cf152f40cc564026ca03b4276b8d46
Lifailon <lifailon@yandex.ru>
2023-07-31 14:37:54 +0300

    test

--- Pipeline/Jenkinsfile.groovy
+++ Pipeline/Jenkinsfile.groovy
@@ -3,9 +3,9 @@ pipeline {
   parameters {
     string(name: "ServiceName", defaultValue: "cron", trim: true, description: "Enter the name of the service")
     choice(name: "System", choices: ["Windows","Linux"], description: "Select the operating system")
-    // booleanParam(name: "ChangeState", defaultValue: false)
+    booleanParam(name: "ChangeState", defaultValue: false)
     choice(name: "State", choices: ["None","Start","Restart","Stop"], description: "None - only get status")
-    choice(name: "StartType", choices: ["None","Enable","Disabled","Manual","Automatic","AutomaticDelayedStart"], description: "Enable mode - for system Linux. Automatic/Manual mode - for system Windows. Delayed Start mode - supported only PowerShell 7")
+    choice(name: "StartType", choices: ["None","Enabled","Disabled","Manual","Automatic","AutomaticDelayedStart"], description: "Enabled mode - for system Linux. Automatic/Manual mode - for system Windows. Delayed Start mode - supported only PowerShell 7")
   }
   stages {
     stage('Powershell via Ansible') {
