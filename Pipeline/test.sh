4e95368ab1afcc8f53c45676ada2237e8db21fa3
Lifailon <lifailon@yandex.ru>
2023-07-31 13:06:21 +0300

    test

--- Pipeline/test.sh
+++ Pipeline/test.sh
@@ -3,6 +3,6 @@ ServiceName=cron;
 echo $State;
 echo $ServiceName;
 systemctl status -a *$ServiceName* | grep -iP "Active:|Loaded:| - ";
-if [[ $State == "Start" ]]; then systemctl start $ServiceName; echo "if start"; fi;
-if [[ $State == "Stop" ]]; then systemctl stop $ServiceName; echo "if stop"; fi;
+if [ $State == "Start" ]; then systemctl start $ServiceName; echo "if start"; fi;
+if [ $State == "Stop" ]; then systemctl stop $ServiceName; echo "if stop"; fi;
 systemctl status   -a *$ServiceName* | grep -iP "Active:|Loaded:| - ";
